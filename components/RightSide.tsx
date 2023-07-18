e380c49180e9ea576e597eb0ec2b1656ad5fb3a4
HAMZA CHBIT <118445750+HamzaChbit@users.noreply.github.com>
2023-07-18 14:25:45 -0700

    Update RightSide.tsx

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -4,7 +4,7 @@ const RightSide = () => {
   return (
     <div className='w-full h-full flex flex-col items-center justify-end text-textLight' >
         <a href="https://mail.google.com/mail/?view=cm&to=hamzachbit@gmail.com" target='_blanc'>
-            <p className='text-sm rotate-90 w-72 tracking-wide text-textGreen' >hamza.chbit.me</p>
+            <p className='text-sm rotate-90 w-72 tracking-wide text-textGreen cursor-pointer  ' >hamza.chbit.me</p>
             </a>
             </div>
   )
