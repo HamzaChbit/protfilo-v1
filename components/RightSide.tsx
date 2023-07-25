461c0073bc1ecb9285a4d2a3b38bceb8af9dfb62
HamzaChbit <hamzachbit@gmail.com>
2023-07-25 13:12:37 +0100

    --ignore-errors

--- components/RightSide.tsx
+++ components/RightSide.tsx
@@ -4,11 +4,16 @@ const RightSide = () => {
   return (
     <div className='w-full h-full flex flex-col items-center justify-end text-textLight' >
         <a href="https://mail.google.com/mail/?view=cm&to=hamzachbit@gmail.com" target='_blanc'>
-<<<<<<< HEAD
+
+
+
             <p className='text-sm rotate-90 w-72 tracking-wide text-textGreen  ' >hamza.chbit.me</p>
-=======
+
             <p className='text-sm rotate-90 w-72 tracking-wide text-textGreen cursor-pointer  ' >hamza.chbit.me</p>
->>>>>>> 317660d9077c11016cd45870fcccc29988f73dd6
+
+
+
+
             </a>
             </div>
   )
