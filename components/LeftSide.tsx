e63c67a785546b66e4e0c72bbea179247e29d3c2
HAMZA CHBIT <118445750+HamzaChbit@users.noreply.github.com>
2023-07-31 12:23:25 +0100

    Update LeftSide.tsx

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -12,7 +12,7 @@ const LeftSide = () => {
               <TbBrandGithub/>
             </span>
           </a>
-          <a href="https://www.linkedin.com/in/hamza-chbit-3b47b1239/t"  target='_blank' >
+          <a href="https://www.linkedin.com/in/hamza-1dz4/"  target='_blank' >
             <span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300' >
               <TiSocialLinkedin/>
             </span>
