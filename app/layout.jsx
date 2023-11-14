abc791ff3e99bf20894c0ebb1baa8ef11032a914
Hamza Chbit <118445750+HamzaChbit@users.noreply.github.com>
2023-11-14 21:32:02 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,21 +7,55 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  metadataBase:new URL('https://hamzachbit.vercel.app/'),
+// export const metadata = {
+//   metadataBase:new URL('https://hamzachbit.vercel.app/'),
   
-  title: {
-    default:'Hamza chbit',
-    template:`%s | hamza chbit`,
+//   title: {
+//     default:'Hamza chbit',
+//     template:`%s | hamza chbit`,
 
-},
+// },
 
 
-  description: 'Hamza chbit  React - Nextjs Front-end Developer (JAMstack)  Talks about #reactjs, #reactdeveloper, and #frontenddevelopment',
+//   description: 'Hamza chbit  React - Nextjs Front-end Developer (JAMstack)  Talks about #reactjs, #reactdeveloper, and #frontenddevelopment',
   
-  verification:{
-    google:"google-site-verification=Ato9kbNjOrYcUtsrS3ym8wBTaKHyfln7WYPC1D2ODcQ"
-  },
+//   verification:{
+//     google:"google-site-verification=Ato9kbNjOrYcUtsrS3ym8wBTaKHyfln7WYPC1D2ODcQ"
+//   },
+//   other: {
+//     'theme-color': '#0d1117',
+//     "color-scheme": "dark only",
+//     "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+//     "twitter:card": "summary_large_image",
+//     "og:url": "hamzachbit.vercel.app",
+//     "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+//     "og:type": "website",
+//   },
+
+
+
+//   metaTags: [
+//     {
+//       name: 'robots',
+//       content: 'all'
+//     },
+//     {
+//       name: 'google-site-verification',
+//       content: 'Ato9kbNjOrYcUtsrS3ym8wBTaKHyfln7WYPC1D2ODcQ'
+//     }
+//   ],
+  
+ 
+
+
+
+
+ 
+// }
+
+export const metadata: Metadata = {
+  title: 'Hamza Chbit ',
+  description: 'I am a FullStack developer with experience building websites and web applications. I specialize in JavaScript, ReactJS, NextJs,ExpressJS , Tailwind CSS ',
   other: {
     'theme-color': '#0d1117',
     "color-scheme": "dark only",
@@ -30,27 +64,7 @@ export const metadata = {
     "og:url": "hamzachbit.vercel.app",
     "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
     "og:type": "website",
-  },
-
-
-
-  metaTags: [
-    {
-      name: 'robots',
-      content: 'all'
-    },
-    {
-      name: 'google-site-verification',
-      content: 'Ato9kbNjOrYcUtsrS3ym8wBTaKHyfln7WYPC1D2ODcQ'
-    }
-  ],
-  
- 
-
-
-
-
- 
+  }
 }
 
 export default function RootLayout({
