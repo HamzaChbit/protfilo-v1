3f101e10af9c948cbd7b182805c4bdf96fbf3ef1
HamzaChbit <hamzachbit@gmail.com>
2023-12-06 11:44:35 +0100

    edit

---
+++ app/sitemap.js
@@ -0,0 +1,11 @@
+export default function sitemap() {
+    return [
+      {
+        url: 'https://hamzachbit.vercel.app',
+        lastModified: new Date(),
+        changeFrequency: 'yearly',
+        priority: 1,
+      },
+    
+    ]
+  }
