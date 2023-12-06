61f39cd5a71e4360b82efab47d2a424fa39dfcf9
HamzaChbit <hamzachbit@gmail.com>
2023-12-06 11:34:10 +0100

    Add file to commit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,56 +1,36 @@
-
-
 import Head from 'next/head'
 import { ThemeProvider } from '../context/ThemeContext'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  metadataBase:new URL('https://hamzachbit.vercel.app/'),
-  
-  title: {
-    default:'Hamza Chbit',
-    template:`%s | hamza chbit`,
+  metadataBase: new URL("https://hamzachbit.vercel.app"),
+  title: 'Hamza Chbit | Chbit  ',
+  description: 'I am a FullStack developer with experience building websites and web applications. I specialize in JavaScript, ReactJS, NextJs,ExpressJS , Tailwind CSS ',
+  applicationName:"Hamza Chbit",
+  keywords:["Hamza Chbit","hamzachbit","hamza chbit","chbit","Hamza chbit"],
+  authors:[{name:"Hamza Chbit"}],
+  creator:"Hamza Chbit",
+  publisher:"Hamza chbit",
+  alternates:{canonical:"/",languages:{"en-US":"/en-US"}  },
 
-},
-
-
-  description: 'Hamza chbit  React - Nextjs Front-end Developer (JAMstack)  Talks about #reactjs, #reactdeveloper, and #frontenddevelopment',
-  
-  verification:{
-    google:"google-site-verification=Ato9kbNjOrYcUtsrS3ym8wBTaKHyfln7WYPC1D2ODcQ"
+  // other: {
+  //   'theme-color': '#0d1117',
+  //   "color-scheme": "dark only",
+  //   "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+  //   "twitter:card": "summary_large_image",
+  //   "og:url": "hamzachbit.vercel.app",
+  //   "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
+  //   "og:type": "website",
+  // },
+  openGraph: {
+    title: 'Hamza Chbit',
+    description: 'I am a FullStack developer with experience building websites and web applications...',
+    url:"https://hamzachbit.vercel.app",siteName:"hamza chbit" ,type:"website",local:"en-US"
   },
-  other: {
-    'theme-color': '#0d1117',
-    "color-scheme": "dark only",
-    "twitter:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
-    "twitter:card": "summary_large_image",
-    "og:url": "hamzachbit.vercel.app",
-    "og:image": 'https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg',
-    "og:type": "website",
-  },
-
-
-
-  metaTags: [
-    {
-      name: 'robots',
-      content: 'all'
-    },
-    {
-      name: 'google-site-verification',
-      content: 'Ato9kbNjOrYcUtsrS3ym8wBTaKHyfln7WYPC1D2ODcQ'
-    }
-  ],
-  
- 
-
-
-
-
- 
 }
 
 export default function RootLayout({
