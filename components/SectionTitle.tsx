c95275d0ec37b0a6f1dd47c4c36c1daedc2c261d
hamza chbit <hamzachbit@gmail.com>
2024-05-30 20:32:01 +0000

    edit

--- components/SectionTitle.tsx
+++ components/SectionTitle.tsx
@@ -9,7 +9,7 @@ interface Props {
 const SectionTitle = ({title,titleNo}:Props) => {
   return (
     <h2 className='font-textFont  text-2xl font-semibold flex items-center' >
-        <span  className='text-base md:text-lg text-textGreen mr-2' >{titleNo}</span>{title}
+        <span  className='text-base md:text-lg text-emerald-800 mr-2' >{titleNo}</span>{title}
         <span  className='hidden md:inline-flex md:w-60 lgl:w-72 h-[.5px] bg-gray-700 ml-6' ></span>
         </h2>
 
