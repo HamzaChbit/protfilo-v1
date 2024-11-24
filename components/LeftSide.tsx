b5fa182a66ab90e1f3d0216c237570b332ca8f50
HamzaChbit <chbithamza1@gmail.com>
2024-11-24 20:51:15 +0100

    edit

--- components/LeftSide.tsx
+++ components/LeftSide.tsx
@@ -13,7 +13,7 @@ const LeftSide = () => {
               <TbBrandGithub/>
             </span>
           </Link>
-          <Link href="https://www.linkedin.com/in/hamza-1dz4/"  target='_blank'
+          <Link href="https://www.linkedin.com/in/hamzachbit/"  target='_blank'
           title='linkedin' >
             <span className='w-10 h-10 text-xl bg-hoverColor rounded-full inline-flex items-center justify-center hover:text-textGreen cursor-pointer hover:translate-y-2 transition-all duration-300' >
               <TiSocialLinkedin/>
